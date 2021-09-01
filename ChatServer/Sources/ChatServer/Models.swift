99930fc12752b63db07c156dc673c5f683a0f41f
sxxrxm <s2019w19@e-mirim.hs.kr>
2021-09-01 12:12:53 +0900

    remove or insert to set if they are disconnected or connected

---
+++ ChatServer/Sources/ChatServer/Models.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by sxxrxm on 2021/09/01.
+//
+
+import Foundation
+
+struct SubmittedChatMessage: Decodable {
+    let message: String
+}
+
+struct ReceivingChatMessage: Encodable, Identifiable {
+    let date = Date()
+    let id = UUID()
+    let message: String
+}
--- ChatServer/Sources/ChatServer/main.swift
+++ ChatServer/Sources/ChatServer/main.swift
@@ -7,8 +7,26 @@ defer{
     app.shutdown()
 }
 
+var clientConnections = Set<WebSocket>()
 app.webSocket("chat"){ req, client in
+    clientConnections.insert(client)
     print("Connected:", client)
+    
+    client.onClose.whenComplete{ _ in
+        clientConnections.remove(client)
+        print("Disconnected:", client)
+    }
 }
 
+
 try app.run()
+
+extension WebSocket: Hashable{
+    public static func  == (lhs: WebSocket, rhs: WebSocket)-> Bool{
+        ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+    
+    public func hash(into hasher: inout Hasher){
+        hasher.combine(ObjectIdentifier(self))
+    }
+}
